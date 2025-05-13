993d8f4300e47a3b2e3191c6f557bacbbf36e344
Sue Wakely <sue.wakely@phs.scot>
2025-05-13 10:50:42 +0000

    added new columns for Left Practice and Main Address Off Shetland

--- read_and_clean_data.R
+++ read_and_clean_data.R
@@ -12,18 +12,8 @@ dir <- path("/conf/LIST_analytics/Shetland/Primary Care/LTC")
 dir_ls(path(dir, "data", "raw")) |>
   path_file()
 
-## Unzip the file to a temporary directory
-#temp_dir <- tempdir()
-#unzip("2025-04 Extract.zip" , exdir = temp_dir)
 
-## List the files in the unzipped directory
-#unzipped_files <- list.files(unzip_dir, full.names = TRUE)
-
-# Read the file (excel)
-#data <- read_excel(file.path(temp_dir, "2025-04-22 - LIST - CV LTC.xlsx "))
-#
-
-#data_file_name <- 025-04-22 - LIST - CV LTC.csv
+#data_file_name <- 2025-04-22 - LIST - CV LTC.csv
 
 raw_data <- read_csv(
   file = path(dir, "data", "raw", "2025-04-22 - LIST - CV LTC.csv"),
@@ -48,6 +38,22 @@ cleaned_filtered <- raw_data |>
   # One record is 1900-01-01 (obvious outlier) other records are in the future
   # We only need the latest ~4 years
   filter(between(EventYear, 1901, year(today())))
+
+
+# Adding a column where EventType = Main Address Off Shetland with a date
+cleaned_filtered %>% 
+  mutate(LeftShetlandDate = if_else(EventType == "Main Address Off Shetland",EventDate,NA)) %>% 
+  group_by(PatientID)%>%
+  mutate(LeftShetlandDate = max(LeftShetlandDate, na.rm = TRUE),
+        LeftShetlandDate=na_if(LeftShetlandDate,as.Date(-Inf))) %>% #clean up when there is no date for LeftShetlandDate
+  ungroup() %>% 
+  mutate(LeftDate = if_else(EventType == "Left Practice",EventDate,NA)) %>% 
+  group_by(PatientID, PracticeID)%>%
+  mutate(LeftDate = max(LeftDate, na.rm = TRUE),
+         LeftDate=na_if(LeftDate,as.Date(-Inf))) %>% #clean up when there is no date for LeftDate
+  ungroup() %>%
+  View()
+
 
 write_parquet(
   cleaned_filtered,
